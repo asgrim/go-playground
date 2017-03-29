6bdf2225e12b9e92037fb8f345a714bfea97c1d2
James Titcumb <james@asgrim.com>
2017-03-29 17:29:36 +0100

    Turn it into a web app

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"github.com/asgrim/go-playground/string"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	reverse := r.URL.Query().Get("reverse")
+
+	if reverse == "" {
+		fmt.Fprint(w, "You need to provide the 'reverse' query parameter...")
+		return
+	}
+
+	fmt.Fprintf(w, "Hey: %s", string.Reverse(reverse))
+}
+
 func main() {
-	fmt.Println(string.Reverse("Hello 👋 😊"))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
